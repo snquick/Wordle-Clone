26a638c0a6bf40ad6618def816e30302858557f5
Stephanie Quick <stephaniequick1997@gmail.com>
2022-10-18 21:57:32 -0500

    finished grid and started keyboard functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import Board from "./Components/Board";
+import Keyboard from "./Components/Keyboard";
+import { boardDefault, generateWordSet } from "./Components/Words";
+import React, { useState, createContext, useEffect } from "react";
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letter: 0 });
+  const [wordSet, setWordSet] = useState(new Set());
+  const [correctWord, setCorrectWord] = useState("");
+  const [disabledLetters, setDisabledLetters] = useState([]);
+  const [gameOver, setGameOver] = useState({
+  });
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider 
+        value = {{
+          board, 
+          setBoard, 
+          setCurrAttempt,
+          correctWord,
+          setDisabledLetters,
+          disabledLetters
+          }} >
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
+      </AppContext.Provider>
+    </div>  
+    );  
 }
 
 export default App;
---
+++ my-app/src/Components/Letter.js
@@ -0,0 +1,11 @@
+import React, { useContext, useEffect } from "react";
+import { AppContext } from "../App";
+
+function Letter({ letterPos, attemptVal }) {
+    const { board } = useContext(AppContext);
+    const letter = board[attemptVal][letterPos];
+    return <div className="letter">{letter}</div>;
+}
+
+export default Letter;
+
